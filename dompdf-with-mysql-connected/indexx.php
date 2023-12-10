dd9a256a5cdc5cc53514d921fadce90c346dd1ef
Manan Dhiman <hi@manandhiman.com>
2023-12-10 17:34:06 +0530

    Added CSS, and background template image

--- dompdf-with-mysql-connected/indexx.php
+++ dompdf-with-mysql-connected/indexx.php
@@ -5,34 +5,31 @@
   $sql = "SELECT * FROM students;";
   $result = $conn->query($sql);
 
-  $table = "<link rel='stylesheet' href='./style.css'/><table><tbody>";
+  $table = "<link rel='stylesheet' href='./style.css'/><table><tbody><tr>";
   $i = 0;
   foreach($result as $student_data) {
-    if($i % 5 == 0) $table .= "</tr><tr>";
+    if($i % 5 == 0 && $i != 0) $table .= "</tr><tr>";
 
     $table .= 
       "<td>
-        <img class='logo' src='./college_logo.png'><br><br>
         <div class='second-section'>
+        <img id='child-image' src='./Images/stud.webp' ><br>
           <div class='child-name'>{$student_data['name']}</div>
           <div class='child-class'>Class: {$student_data['class']}</div>
           <div class='child-details'>
-            <span class='red_details'>F.Name</span>: {$student_data['f_name']}<br>
-            <span class='red_details'>M.Name</span>: {$student_data['m_name']}<br>
-            <span class='red_details'>Mob. No</span>: {$student_data['class']}<br>
-            <span class='red_details'>Adm. No</span> : {$student_data['admission_no']}
-            <span class='red_details'>D.O.B</span> : {$student_data['dob']}<br>
-            <span class='red_details'>Address</span>: {$student_data['address']}<br>
-            <br><br>
-            <img class='logo' src='./sign.png'><br>
-            <span>Principal</span>
+            <span class='red-details space-right'>F.Name</span>: {$student_data['f_name']}<br>
+            <span class='red-details'>M.Name</span>: {$student_data['m_name']}<br>
+            <span class='red-details'>Mob. No</span>: {$student_data['class']}<br>
+            <span class='red-details'>Adm. No</span> : {$student_data['admission_no']}
+            <span class='red-details space-left'>D.O.B</span> : {$student_data['dob']}<br>
+            <span class='red-details'>Address</span>: {$student_data['address']}<br>
           </div>
         </div>
       </td>";
     $i++;
   } 
 
-  $table .= "</table></tbody>";
+  $table .= "</tr></tbody></table>";
 
   // require_once 'dompdf/autoload.inc.php';
   require 'vendor/autoload.php';
@@ -41,11 +38,10 @@
   // instantiate and use the dompdf class
   $dompdf = new Dompdf();
 
-
   $html_content = $table;
   // echo $html_content;
 
-  
+  $dompdf->set_option("dpi", 1000);
   $dompdf->getOptions()->setChroot($_SERVER["DOCUMENT_ROOT"]); 
   $dompdf->loadHtml($html_content);
   // (Optional) Setup the paper size and orientation
